857629e7fa5880ab12812c187319bfa6acc61b7e
Kyle Hersey <nethermole@gmail.com>
2022-12-11 15:23:20 -0800

    added game seeds for replayability, fixed python script, niceified API interface

--- python/playgame.py
+++ python/playgame.py
@@ -3,32 +3,23 @@ import requests
 def toChars(word):
     return [char for char in word]
 
-url = "http://localhost:8080"
-
-requests.post(url+ "/debugStart")
+requests.post("http://localhost:8080/debugStart")
 
 go = True
 while go:
-    status = requests.post(url + "/checkUpdate").json()
-    if status['value'] == "Waiting for hand submission":
-        print("Please submit your hand: ")
+    cards = requests.get("http://localhost:8080/player/0/getHand").json()
+    for i in range(len(cards)):
+        print(str(i) + "    " + str(cards[i]))
+    numberChoices = input("Which cards do you want?")
+    indexes = toChars(numberChoices)
+    cardChoices = []
+    for i in range(5):
+        cardChoices.append(cards[int(numberChoices[i])])
 
-        cards = requests.get(url + "/player/0/getHand").json()
-        for i in range(len(cards)):
-            print(str(i) + "    " + str(cards[i]))
-        numberChoices = input("Which cards do you want?\n")
-
-        indexes = toChars(numberChoices)
-        cardChoices = []
-        for i in range(5):
-            cardChoices.append(cards[int(numberChoices[i])])
-        requests.post(url + "/player/0/submitHand", json=cardChoices)
-        print("Submitted hand:")
-        for i in range(len(cardChoices)):
-            print(str(cardChoices[i]))
-
-    else:
-        print(status['value'])
+    requests.post("http://localhost:8080/player/0/submitHand", json=cardChoices)
+    print("Submitted hand:")
+    for i in range(len(cardChoices)):
+        print(str(cardChoices[i]))
 
     go = input("Press n to stop") != "n"
 
